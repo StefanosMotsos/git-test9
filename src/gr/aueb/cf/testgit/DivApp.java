ce13f5db44b62a24c9c9ad908613b5a2bfbd17f9
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2025-12-02 23:39:42 +0200

    feat: add divapp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class DivApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
